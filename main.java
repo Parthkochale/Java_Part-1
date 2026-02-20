52c2025c990fc8547b357401072d3d447763a906
Parth Kochale <piyushkochale5@gmail.com>
2026-02-20 23:32:13 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,23 @@
+Class FirstClass {
+    public static void main(String[] args){
+      System.out.println("Hello World");
+    }
+}
+//output: Hello World
+
+public class Main{
+   public static void main(String[] args){
+    //variables 
+    int a = 10;
+    int b = 20;
+    int sum = a+b;
+    System.out.println(sum);
+    int diff= b-a;
+    System.out.println(diff);
+    int mul= a*b;
+    System.out.println(mul);
+   }
+}
+//output: 30
+//        10
+//        200
